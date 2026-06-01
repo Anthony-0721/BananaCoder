291b5c19617df51eca4cc86e4893741487540d3e
BananaCoder <banana@local.dev>
2026-06-01 21:05:15 +0800

    chore: remove reference projects (CoreCoder, chcode, nanobot)

--- chcode/chcode/__main__.py
+++
@@ -1,5 +0,0 @@
-"""python -m chcode 入口"""
-from chcode.cli import app
-
-if __name__ == "__main__":
-    app()
--- chcode/chcode/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from chcode.utils.text_utils import get_text_content, mask_api_key
-
-__all__ = ["get_text_content", "mask_api_key"]
